8dc908e9bae50d58238fc2d615f9d5efeb082640
Rodrigo Vela <rodrigo.vela@softlive.com.br>
2024-05-29 19:19:24 -0400

    tests(task): adding wrong path to e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateTaskDto } from '../src/modules/task/dto/create-task.dto';
+import { TaskStatus, TaskPriority } from '../src/modules/task/dto/task.enum';
 
 describe('Tasks API (e2e)', () => {
   let app: INestApplication;
@@ -19,115 +21,180 @@ describe('Tasks API (e2e)', () => {
     await app.close();
   });
 
-  it('/task (POST) - Create a task', () => {
-    return request(app.getHttpServer())
-      .post('/task')
-      .send({
-        title: 'Task',
+  describe('/task (POST)', () => {
+    it('Create a task', () => {
+      return request(app.getHttpServer())
+        .post('/task')
+        .send({
+          title: 'Task',
+          description: 'Task description',
+          status: 'to-do',
+          priority: 'low',
+        })
+        .expect(201)
+        .then(response => {
+          expect(response.body).toHaveProperty('id');
+          expect(response.body.title).toEqual('Task');
+          expect(response.body.description).toEqual('Task description');
+          expect(response.body.status).toEqual('to-do');
+          expect(response.body.priority).toEqual('low');
+          expect(response.body).toHaveProperty('createdAt');
+          expect(response.body).toHaveProperty('updatedAt');
+        });
+    });
+  
+    it('Create a task only with title and description', () => {
+      return request(app.getHttpServer())
+        .post('/task')
+        .send({
+          title: 'Task',
+          description: 'Task description',
+        })
+        .expect(201)
+        .then(response => {
+          expect(response.body).toHaveProperty('id');
+          expect(response.body.title).toEqual('Task');
+          expect(response.body.description).toEqual('Task description');
+          expect(response.body.status).toEqual('to-do');
+          expect(response.body.priority).toEqual('low');
+          expect(response.body).toHaveProperty('createdAt');
+          expect(response.body).toHaveProperty('updatedAt');
+        });
+    });
+
+    it('Should return 400 if title is missing', async () => {
+      const createTaskDto = {
         description: 'Task description',
         status: 'to-do',
         priority: 'low',
-      })
-      .expect(201)
-      .then(response => {
-        expect(response.body).toHaveProperty('id');
-        expect(response.body.title).toEqual('Task');
-        expect(response.body.description).toEqual('Task description');
-        expect(response.body.status).toEqual('to-do');
-        expect(response.body.priority).toEqual('low');
-        expect(response.body).toHaveProperty('createdAt');
-        expect(response.body).toHaveProperty('updatedAt');
-      });
-  });
+      };
 
-  it('/task (POST) - Create a task only with title and description', () => {
-    return request(app.getHttpServer())
-      .post('/task')
-      .send({
-        title: 'Task',
+      const response = await request(app.getHttpServer())
+        .post('/task')
+        .send(createTaskDto)
+        .expect(400);
+
+      expect(response.body.message).toContain('title should not be empty');
+    });
+
+    it('Should return 400 if description is missing', async () => {
+      const createTaskDto = {
+        title: 'Task Title',
+        status: 'to-do',
+        priority: 'low',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/task')
+        .send(createTaskDto)
+        .expect(400);
+
+      expect(response.body.message).toContain('description should not be empty');
+    });
+
+    it('Should return 400 if status is invalid', async () => {
+      const createTaskDto = {
+        title: 'Task Title',
         description: 'Task description',
-      })
-      .expect(201)
-      .then(response => {
-        expect(response.body).toHaveProperty('id');
-        expect(response.body.title).toEqual('Task');
-        expect(response.body.description).toEqual('Task description');
-        expect(response.body.status).toEqual('to-do');
-        expect(response.body.priority).toEqual('low');
-        expect(response.body).toHaveProperty('createdAt');
-        expect(response.body).toHaveProperty('updatedAt');
-      });
+        status: 'invalid-status',
+        priority: 'low',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/task')
+        .send(createTaskDto)
+        .expect(400);
+
+      expect(response.body.message).toContain('status must be a valid enum value');
+    });
+
+    it('Should return 400 if priority is invalid', async () => {
+      const createTaskDto = {
+        title: 'Task Title',
+        description: 'Task description',
+        status: 'to-do',
+        priority: 'invalid-priority',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/task')
+        .send(createTaskDto)
+        .expect(400);
+
+      expect(response.body.message).toContain('priority must be a valid enum value');
+    });
   });
 
-  it('/task (GET) - List all tasks', () => {
-    return request(app.getHttpServer())
-      .get('/task')
-      .expect(200)
-      .then(response => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-      });
+
+  describe('/task (GET)', () => {
+    it('List all tasks when no tasks exist', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/task')
+        .expect(200);
+
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBe(0);
+    });
+
+    it('Get a specific task that does not exist', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/task/999')
+        .expect(404);
+
+      expect(response.body.message).toContain('Task not found');
+    });
   });
 
-  it('/task/:id (GET) - Get a specific task', () => {
-    return request(app.getHttpServer())
-      .get('/task/1')
-      .expect(200)
-      .then(response => {
-        expect(response.body).toHaveProperty('id');
-        expect(response.body).toHaveProperty('title');
-        expect(response.body).toHaveProperty('description');
-        expect(response.body).toHaveProperty('status');
-        expect(response.body).toHaveProperty('priority');
-        expect(response.body).toHaveProperty('createdAt');
-        expect(response.body).toHaveProperty('updatedAt');
-      });
-  });
-
-  it('/task/:id (PUT) - Update a task', () => {
-    return request(app.getHttpServer())
-      .patch('/task/1')
-      .send({
+  describe('/task/:id (PATCH)', () => {
+    it('Should return 404 if task does not exist', async () => {
+      const updateTaskDto = {
         title: 'Updated Task',
         description: 'Updated description',
         status: 'in-progress',
         priority: 'high',
-      })
-      .expect(200)
-      .then(response => {
-        expect(response.body).toHaveProperty('id');
-        expect(response.body.title).toEqual('Updated Task');
-        expect(response.body.description).toEqual('Updated description');
-        expect(response.body.status).toEqual('in-progress');
-        expect(response.body.priority).toEqual('high');
-        expect(response.body).toHaveProperty('createdAt');
-        expect(response.body).toHaveProperty('updatedAt');
-      });
+      };
+
+      const response = await request(app.getHttpServer())
+        .patch('/task/999')
+        .send(updateTaskDto)
+        .expect(404);
+
+      expect(response.body.message).toContain('Task not found');
+    });
+
+    it('Should return 400 if data is invalid', async () => {
+      const createTaskDto: CreateTaskDto = {
+        title: 'Initial Task',
+        description: 'Initial description',
+        status: TaskStatus.TO_DO,
+        priority: TaskPriority.LOW,
+      };
+
+      const { body: createdTask } = await request(app.getHttpServer())
+        .post('/task')
+        .send(createTaskDto)
+        .expect(201);
+
+      const updateTaskDto = {
+        status: 'invalid-status',
+      };
+
+      const response = await request(app.getHttpServer())
+        .patch(`/task/${createdTask.id}`)
+        .send(updateTaskDto)
+        .expect(400);
+
+      expect(response.body.message).toContain('status must be a valid enum value');
+    });
   });
 
-  it('/task/:id (PATCH) - Update a task with partial data', () => {
-    return request(app.getHttpServer())
-      .patch('/task/1')
-      .send({
-        status: 'done',
-      })
-      .expect(200)
-      .then(response => {
-        expect(response.body).toHaveProperty('id');
-        expect(response.body).toHaveProperty('title');
-        expect(response.body).toHaveProperty('description');
-        expect(response.body.status).toEqual('done');
-        expect(response.body).toHaveProperty('priority');
-        expect(response.body).toHaveProperty('createdAt');
-        expect(response.body).toHaveProperty('updatedAt');
-      });
-  });
+  describe('/task/:id (DELETE)', () => {
+    it('should return 404 if task does not exist', async () => {
+      const response = await request(app.getHttpServer())
+        .delete('/task/999')
+        .expect(404);
 
-  it('/task/:id (DELETE)', () => {
-    return request(app.getHttpServer())
-      .delete('/task/1')
-      .expect(200)
-      .then(response => {
-        expect(response.body).toEqual(null);
-      });
+      expect(response.body.message).toContain('Task not found');
+    });
   });
 });
